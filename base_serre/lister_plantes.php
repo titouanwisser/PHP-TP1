7383e5edc9a36ad3c1cdb2e1fbdf54d6dbffd91f
Titouan WISSER <titouanwisser@gmail.com>
2024-11-15 15:01:16 +0100

    Lister plantes par région FIN

---
+++ base_serre/lister_plantes.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once('connexion_serre.php');
+
+$stmt = $connexion->prepare("SELECT * FROM plante WHERE noregion=:noregion");
+
+$noregion = $_GET['noregion'];
+$stmt->bindValue(":noregion", $noregion);
+$stmt->setFetchMode(PDO::FETCH_OBJ);
+$stmt->execute();
+
+echo "<table border=2>";
+
+while($enregistrement = $stmt->fetch())
+
+{
+
+    echo "<tr>";
+  // Affichage des champs de la table plante en fonction de la region
+    echo '<td>', $enregistrement->noplante, '</td>';
+    echo '<td>', $enregistrement->nomplante, '</td>';
+    echo "</tr>";
+
+}
+
+echo "</table>";
+
+?>
